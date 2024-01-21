e1fa05283ea35c6c28d5da199eeaa36129336a9e
Owen175 <oclocliphone@icloud.com>
2024-01-21 17:43:58 +0000

    cleaned up the function and added comments

--- wagner-fischer.py
+++ wagner-fischer.py
@@ -1,80 +1,84 @@
 class Spellcheck:
-    def __init__(self, numwords=10000):
-        self.words_filename = 'words.txt'
-        self.numwords = numwords
-        self.spelling_filename = 'spellcorrection.txt'
-        self.extended_words_filename = 'extended_words.txt'
-        self.correctly_spelt_words = self.load_words()
+    def __init__(self, numwords=10000, words_filename='words.txt', extended_words_filename='extended_words.txt',
+                 correction_filename='spellcorrection.txt'):
+        self.__words_filename = words_filename
+        self.__extended_words_filename = extended_words_filename
+        # words contains 10,000 words, whereas extended_words contains 333,000
+        # both are ordered by use frequency, meaning that the output is first by edit distance, second by use frequency
+        self.__numwords = numwords
+        # number of words to compare against
+        self.__spelling_filename = correction_filename
+        self.__correctly_spelt_words = self.__load_words()
 
-    def load_words(self):
+    def __update_text_file(self, rankings):
+        with open(self.__spelling_filename, 'w') as f:
+            [f.write(f'{r[0]} : {r[1]}\n') for r in rankings]
+
+    def __load_words(self):
         wds = []
-        if self.numwords <= 10000:
-            filename = self.words_filename
+        if self.__numwords <= 10000:
+            filename = self.__words_filename
+            # uses the smaller file for efficiency if it can be used so that you don't need to
+            # download the larger file unless you need the extra words
         else:
-            filename = self.extended_words_filename
+            filename = self.__extended_words_filename
+
         with open(filename, 'r') as f:
             for i, line in enumerate(f):
-                if i > self.numwords - 1:
+                if i > self.__numwords - 1:
                     return wds
                 wds.append(line.strip())
+        # this means that if over 330,000 words are requested, it caps at 330,000
         return wds
-    def insert_word(self, rankings, array):
+
+    @staticmethod
+    def __insert_word(rankings, array):
         dist = array[1]
         if len(rankings) == 0:
             return [array]
         for i, (_, y) in enumerate(rankings):
             if y > dist:
                 return rankings[:i] + [array] + rankings[i:]
+        # returns at the raw length - is adjusted later if the array needs to be a certain length
         return rankings + [array]
-    def get_dist(self, ls):
-        return ls[-1]
 
-    def spellcheck_word(self, word, num_words_returned, writeToTextFile=False):
-        if word in self.correctly_spelt_words:
+    def spellcheck_word(self, word, num_words_returned=10, write_to_text_file=False):
+        if word in self.__correctly_spelt_words:
             return word
         else:
             rankings = []
-            for destination in self.correctly_spelt_words:
-                dist = self.calculate_distance(word, destination)
+            for destination in self.__correctly_spelt_words:
+                dist = self.calculate_edit_distance(word, destination)
+                # the number of insertions, deletions and changes of letters summed
                 if len(rankings) < num_words_returned:
-                    rankings = self.insert_word(rankings, [destination, dist])
-                    if writeToTextFile:
-                        self.update_text_file(rankings)
+                    rankings = self.__insert_word(rankings, [destination, dist])
+                    if write_to_text_file:
+                        self.__update_text_file(rankings)
                 elif dist < rankings[-1][1]:
-                    rankings = self.insert_word(rankings, [destination, dist])[:num_words_returned]
-                    if writeToTextFile:
-                        self.update_text_file(rankings)
-        return rankings
-
-    def update_text_file(self, rankings):
-        with open(self.spelling_filename, 'w') as f:
-            for r in rankings:
-                f.write(r[0])
-                f.write(' : ')
-                f.write(str(r[1]))
-                f.write('\n')
+                    rankings = self.__insert_word(rankings, [destination, dist])[:num_words_returned]
+                    if write_to_text_file:
+                        self.__update_text_file(rankings)
+            return rankings
 
     @staticmethod
-    def calculate_distance(word, destination):
-        if word == '' or destination == '':
+    def calculate_edit_distance(word, destination):
+        if not(word and destination):   # using the strings as booleans - len of 0 is False.
             return max(len(word), len(destination))
+
         word = '_' + word
         destination = '_' + destination
-        wordlen, destinationlen = len(word), len(destination)
-        pastLine = list(range(destinationlen))
-        for i in range(1, wordlen):
-            currentLine = [i]
-            for j in range(1, destinationlen):
-                change_letter = pastLine[j - 1]
-                add_letter = pastLine[j] + 1
-                delete_letter = currentLine[j - 1] + 1
+        word_len, destination_len = len(word), len(destination)
+        past_line = list(range(destination_len))
+
+        # This uses the wagner-fischer method
+        for i in range(1, word_len):
+            current_line = [i]
+            for j in range(1, destination_len):
+                change_letter = past_line[j - 1]
+                add_letter = past_line[j] + 1
+                delete_letter = current_line[j - 1] + 1
                 if word[i] != destination[j]:
                     change_letter += 1
-                currentLine.append(min(change_letter, add_letter, delete_letter))
-            pastLine = currentLine
-        return currentLine[-1]
-
-
-spc = Spellcheck()
-print(spc.spellcheck_word('oogabooga', 10))
-print(29554718100/100000000000)
+                current_line.append(min(change_letter, add_letter, delete_letter))
+            past_line = current_line
+        return current_line[-1]
